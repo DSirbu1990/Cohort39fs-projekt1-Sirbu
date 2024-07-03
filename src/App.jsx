5846b7e3dc805c442182280f94595788ff113423
DSirbu1990 <doina.strilciuc@gmail.com>
2024-07-03 17:53:15 +0200

    lesson05 commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import "./App.css";
 import Lesson02 from "./lessons/Lesson02/Lesson02";
 import Lesson03 from "./lessons/Lesson03/Lesson03";
 import Lesson04 from "./lessons/Lesson04/Lesson04";
+import Lesson05 from "./lessons/Lesson05/Lesson05";
 
 //Homeworks
 import Homework02 from "./homeworks/Homework02/Homework02";
 import Homework03 from "./homeworks/Homework03/Homework03";
+import Homework04 from "./homeworks/Homework04/Homework04";
 
 
 
@@ -16,9 +18,10 @@ function App() {
     <div className="App">
       {/* <Homework02 /> */}
       {/* <Homework03 /> */}
+      <Lesson05 />
       {/* <Lesson02 /> */}
       {/* <Lesson03 /> */}
-      <Lesson04 />
+      {/* <Lesson04 /> */}
     </div>
   );
 }
